726bc63f29dcf0447ed388d10d59b6223571e3fe
ch15 <ch15solito@gmail.com>
2024-02-22 00:00:38 +0800

    rest api crud laravel

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserStoreRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'results' => $users
+        ], 200);
+    }
+
+    public function store(UserStoreRequest $request)
+    {
+        try {
+
+            User::create([
+
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            return response()->json([
+                'message' => "User successfully Created."
+            ], 200);
+
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'message' => "Something went wrong"
+            ], 500);
+        }
+    }
+
+    public function show($id)
+    {
+
+        $users = User::find($id);
+        if (!$users) {
+            return response()->json([
+                'message' => 'User Not Found.'
+            ], 404);
+        }
+
+        return response()->json([
+            'users' => $users
+        ], 200);
+    }
+
+    public function update(UserStoreRequest $request, $id)
+    {
+        try{
+
+            $users = User::find($id);
+
+            if (!$users) {
+                return $users->json([
+                    'message' => 'User Not Found.'
+                ], 404);
+            }
+
+            $users->name = $request->name;
+            $users->email = $request->email;
+            $users->password = $request->password;
+
+            $users->save();
+
+            return response()->json([
+                'message' => "User successfully Updated."
+            ], 200);
+
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Something went Wrong!.'
+            ], 500);
+        }
+    }
+
+    public function destroy($id){
+
+        $users = User::find($id);
+        if (!$users) {
+            return response()->json([
+                'message' => 'User Not Found.'
+            ], 404);
+        }
+
+        $users->delete();
+
+         return response()->json([
+                'message' => "User successfully Deleted."
+            ], 200);
+    }
+}
